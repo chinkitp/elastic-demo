00117063ec92e85bd8aea9d4ac975b982667dea3
Chinkit Patel <chinkit.patel@unisa.edu.au>
2018-04-28 16:32:01 +0930

    ingestory working

--- client/src/ingestor/Program.cs
+++ client/src/ingestor/Program.cs
@@ -1,30 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using Nest;
+using System.Threading;
+using System.Linq;
+using Newtonsoft.Json;
+
 
 namespace ingestor
 {
+    public class EpgProperties : Dictionary<string, object>
+    {
+
+    }
+
+    public class EpgVertex
+    {
+        public string Id {get;}
+        public string Label {get;}
+        public EpgProperties EpgProperties {get;}
+
+        public EpgVertex(string id, string label, EpgProperties epgProperties)
+        {
+            Id = id;
+            Label = label;
+            EpgProperties = epgProperties;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            ElasticClient client = new ElasticClient(new Uri("http://elastic:9200"));
-
-            
-            var created = client.CreateIndex("hello-world-v1", i => i
-				.Settings(s => s
-					.NumberOfShards(2)
-					.NumberOfReplicas(0)
-				)
-
-			);
-
-            Console.WriteLine($"The index was {created}");
+            ElasticClient client = new ElasticClient(new Uri("http://localhost:9200"));
             
 
+            IEnumerable<string> vertices = System.IO.File
+            .ReadAllLines("/Users/chinkit/00D2D-CRC/01-Projects/data61/github/stellar-search/search/src/test/resources/au/csiro/data61/stellar/search/epg/imdb-flat/imdb-vertices.json");
+
+            var docs = vertices.Select(v => JsonConvert.DeserializeObject(v));
+
+            // var result = client.IndexMany(docs,"imdb-v3");
+            // Console.WriteLine(result.DebugInformation);
+            Console.WriteLine("Indexing documents into elasticsearch...");
+            var waitHandle = new CountdownEvent(1);
+
+
+
+            var bulkAll = client.BulkAll(docs, b => b
+                .Index("imdb-v4")
+                //.Type("my-console")
+                .BackOffRetries(2)
+                .BackOffTime("30s")
+                .RefreshOnCompleted(true)
+                .MaxDegreeOfParallelism(4)
+                .Size(2)
+            );
+
+            bulkAll.Subscribe(new BulkAllObserver(
+                onNext: (b) => {
+                     Console.Write("."); 
+                     },
+                onError: (e) => { throw e; },
+                onCompleted: () =>  waitHandle.Signal()
+            ));
+            
+            waitHandle.Wait();
+            Console.WriteLine("Done.");
 
             Console.ReadLine();
             
            
         }
+            
     }
 }
