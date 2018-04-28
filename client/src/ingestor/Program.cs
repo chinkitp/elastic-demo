0ef25d175a30e897ed10218a0f80a12ea4df31e5
Chinkit Patel <chinkit.patel@unisa.edu.au>
2018-04-28 11:31:55 +0930

    adding client

---
+++ client/src/ingestor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ingestor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
